e9899e435fbd64e45a50c204721ea8e9901c3200
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2017-03-31 05:39:53 +0300

    compilation fixed

--- GslNet.Tests/MultiMin/FdfMinimizerTests.cs
+++ GslNet.Tests/MultiMin/FdfMinimizerTests.cs
@@ -19,11 +19,7 @@ namespace GslNet.Tests.MultiMin
             vector[0] = 1;
             vector[1] = 1;
             FdfMinimizer minimizer = new FdfMinimizer(AlgorithmWithDerivatives.ConjugatePR, 2);
-            Fdf fdf = new Fdf(v => v[0] * v[0] + v[1] * v[1], delegate(GslVector arg, GslVector result)
-            {
-                result[0] = 2 * arg[0];
-                result[1] = 2 * arg[1];
-            }, 2);
+            Fdf fdf = new Fdf(new Circle(), 2);
             minimizer.Initialize(fdf, vector, 1, 0.1);
             for (int i = 0; i < 2; i++)
             {
@@ -31,6 +27,26 @@ namespace GslNet.Tests.MultiMin
             }
             Assert.AreEqual(0, minimizer.X[0], 1e-15);
             Assert.AreEqual(0, minimizer.X[1], 1e-15);
+        }
+
+        private class Circle : IFunctionWithGradient
+        {
+            public double Evaluate(GslVector argument)
+            {
+                return argument[0] * argument[0] + argument[1] * argument[1];
+            }
+
+            public void EvaluateGradient(GslVector argument, GslVector result)
+            {
+                result[0] = 2 * argument[0];
+                result[1] = 2 * argument[1];
+            }
+
+            public double EvaluateValueWithGradient(GslVector argument, GslVector result)
+            {
+                EvaluateGradient(argument, result);
+                return Evaluate(argument);
+            }
         }
     }
 }
