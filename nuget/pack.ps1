c3a66427ca45564ae53018211f53821d52db8a21
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2017-03-31 10:41:07 +0300

    optimisic nuget packaging

---
+++ nuget/pack.ps1
@@ -0,0 +1,12 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$version = [System.Reflection.Assembly]::LoadFile("$root\GslNet\bin\Debug\MarkdownLog.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\NuGet\GslNet.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\nuget\GslNet.compiled.nuspec
+
+& $root\NuGet\NuGet.exe pack $root\nuget\GslNet.compiled.nuspec
